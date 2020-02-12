6584632518b0f6ca24e67ac5558891e7e9ee69d6
Sheldon Malboeuf <smalboeuf01@gmail.com>
2020-02-12 22:00:42 +0000

    Organized code and output

--- fetcher.js
+++ fetcher.js
@@ -5,11 +5,6 @@ const fs = require('fs');
 args.splice(0, 2);
 
 
-const outputMessage = (bytes, PATH) => {
-
-  return `Downloaded and saved ${bytes} bytes to ${PATH}`
-};
-
 const fetcher = function(args) {
 
   const URL = args[0];
@@ -25,22 +20,22 @@ const fetcher = function(args) {
         
         fs.stat(PATH, (err, stats) => {
           
-          if(err) {
+          if (err) {
             console.log(err);
           } else {
-            // return `Downloaded and saved ${stats.size} bytes to ${PATH}`;
-            return outputMessage(stats.size, PATH);
+           
+            console.log(`Downloaded and saved ${stats.size} bytes to ${PATH}`);
 
           }
-        })
+        });
       }
     });
 
     
   });
 
-
-}
+  return "";
+};
 
 
 
